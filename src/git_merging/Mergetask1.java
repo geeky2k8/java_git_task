66725f232f6be393a270e94a22610ef457169e69
Dinesh Dandu <dinesh@Dineshs-MacBook-Pro.local>
2016-11-15 14:54:01 -0800

    adding fnOne

---
+++ src/git_merging/Mergetask1.java
@@ -0,0 +1,14 @@
+package git_merging;
+
+public class Mergetask1 {
+	public void fnOne() {
+		System.out.println("I could not repair your breaks, so I made your horn louder.");
+	}
+
+	public static void main(String[] args) {
+		Mergetask1 mt = new Mergetask1();
+		mt.fnOne();
+
+	}
+
+}
