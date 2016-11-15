129c8662622715f4c0c6d26843b48cf014179112
Dinesh Dandu <dinesh@Dineshs-MacBook-Pro.local>
2016-11-15 15:41:30 -0800

    New thought for the day

--- src/git_merging/Mergetask1.java
+++ src/git_merging/Mergetask1.java
@@ -2,7 +2,7 @@ package git_merging;
 
 public class Mergetask1 {
 	public void thoughtFortheDay() {
-		System.out.println("I could not repair your breaks, so I made your horn louder.");
+		System.out.println("On the other hand you have different fingers.");
 	}
 
 	public static void main(String[] args) {
