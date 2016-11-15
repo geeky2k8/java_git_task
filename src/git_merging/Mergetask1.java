cd7d7a18d1ebad24b98d279d1b4278e3c0ae52a7
Dinesh Dandu <dinesh@Dineshs-MacBook-Pro.local>
2016-11-15 15:06:54 -0800

    Added events

--- src/git_merging/Mergetask1.java
+++ src/git_merging/Mergetask1.java
@@ -4,10 +4,15 @@ public class Mergetask1 {
 	public void thoughtFortheDay() {
 		System.out.println("I could not repair your breaks, so I made your horn louder.");
 	}
+	
+	public void upcomingEvents() {
+		System.out.println("Party at Jane's house tomorrow");
+	}
 
 	public static void main(String[] args) {
 		Mergetask1 mt = new Mergetask1();
 		mt.thoughtFortheDay();
+		mt.upcomingEvents();
 
 	}
 
